46c81767085ea73292bf859e8bfcb7141c64bcc4
Gaelle Albertus <gaelle.albertus@gmail.com>
2020-09-02 15:40:05 +0200

    Additional exercises (warning: it raises an exception !)

--- model.py
+++ model.py
@@ -8,9 +8,6 @@ class Agent:
         for attr_name, attr_value in agent_attributes.items():
             setattr(self, attr_name, attr_value)
 
-    def say_hello(self, first_name):
-        return "Bien le bonjour " + first_name + " !"
-
 
 class Position:
 
@@ -28,41 +25,76 @@ class Position:
 
 
 class Zone:
-
+    ZONES = []
     MIN_LONGITUDE_DEGREES = -180 # Attribut de classe
     MAX_LONGITUDE_DEGREES = 180
     MIN_LATITUDE_DEGREES = -90
     MAX_LATITUDE_DEGREES = 90
     WIDTH_DEGREES = 1
     HEIGHT_DEGREES = 1
-    ZONES = []
 
     def __init__(self, corner1, corner2):
-        self.corner1 = corner1 # Attribut d'instance
+        self.corner1 = corner1 # Attribut d'instance (toujours dans __init__)
         self.corner2 = corner2
-        self.inhabitants = 0
+        self.inhabitants = []
+
+    @property
+    def population(self):
+        return len(self.inhabitants)
+
+    def add_inhabitant(self, inhabitant):
+        self.inhabitants.append(inhabitant)
+
+    def contains(self, position):
+        """Return True if the zone contains this position"""
+        return position.longitude >= min(self.corner1.longitude, self.corner2.longitude) and \
+            position.longitude < max(self.corner1.longitude, self.corner2.longitude) and \
+            position.latitude >= min(self.corner1.latitude, self.corner2.latitude) and \
+            position.latitude < max(self.corner1.latitude, self.corner2.latitude)
+
+    @classmethod
+    def find_zone_that_contains(cls, position):
+
+        longitude_index = int((position.longitude_degrees - cls.MIN_LONGITUDE_DEGREES) / cls.WIDTH_DEGREES)
+        latitude_index = int((position.latitude_degrees - cls.MIN_LATITUDE_DEGREES) / cls.HEIGHT_DEGREES)
+        longitude_bins = int((cls.MAX_LONGITUDE_DEGREES - cls.MIN_LONGITUDE_DEGREES) / cls.WIDTH_DEGREES)
+        zone_index = latitude_index * longitude_bins + longitude_index
+
+        zone = cls.ZONES[zone_index]
+
+        print(zone_index)
+        assert zone.contains(position) # raise an exception when false
+
+        return zone
+
+
 
     # Méthode globale à la classe (et non de l'instance)
     @classmethod
-    def initialize_zones(cls):
+    def _initialize_zones(cls):
         for latitude in range(cls.MIN_LATITUDE_DEGREES, cls.MAX_LATITUDE_DEGREES, cls.WIDTH_DEGREES):
             for longitude in range(cls.MIN_LONGITUDE_DEGREES, cls.MAX_LONGITUDE_DEGREES, cls.WIDTH_DEGREES):
                 bottom_left_corner = Position(longitude, 1)
                 top_right_corner = Position(longitude + cls.WIDTH_DEGREES, latitude + cls.HEIGHT_DEGREES)
                 zone = Zone(bottom_left_corner, top_right_corner)
                 cls.ZONES.append(zone)
-        print(len(cls.ZONES))
 
+        print("Length of ZONES is: " + str(len(cls.ZONES)))
 
 def main():
+
+    Zone._initialize_zones()
+
     for agent_attributes in json.load(open("agents-100k.json")):
         longitude = agent_attributes.pop("longitude")
         latitude = agent_attributes.pop("latitude")
-        position = Position(longitude, latitude)
-        agent = Agent(position, **agent_attributes)
-        # print(agent.agreeableness)
-        # print(agent.position.longitude)
-    Zone.initialize_zones()
 
+        position = Position(longitude, latitude)
+
+        agent = Agent(position, **agent_attributes)
+        zone = Zone.find_zone_that_contains(position)
+        zone.add_inhabitant(agent)
+
+        print("Zone population: ", zone.population)
 
 main()
